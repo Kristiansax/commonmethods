a11848b0f7e736d837516f5f595c955dafeb1842
Kristiansax <krellesax@hotmail.com>
2020-10-19 22:19:03 +0200

    First commit of local repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Common Methods
+
+'commonmethods' is a python library that contains methods that I use often, and don't want to rewrite for every project.
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+	name='commonmethods',
+	version='0.0.1',
+	description='This library contains some common methods that you don\'t wanna rewrite every time you make a new project.',
+	py_modules=['commonmethods'],
+	package_dir={'':'src'}
+)
